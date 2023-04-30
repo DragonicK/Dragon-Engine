5607d7278e1a79f9fe864cd2491fca49e2212e39
Julio Sperandio <juliosperandio@hotmail.com>
2023-04-29 23:09:15 -0300

    Refactoring SkillManager.

--- Dragon.Game/Manager/SkillManager.cs
+++ Dragon.Game/Manager/SkillManager.cs
@@ -1,80 +1,84 @@
 ﻿using Dragon.Core.Model;
 using Dragon.Core.Content;
+using Dragon.Core.Services;
 using Dragon.Core.Model.Items;
 using Dragon.Core.Model.Skills;
 using Dragon.Core.Model.Passives;
 
 using Dragon.Game.Players;
-using Dragon.Game.Network;
 using Dragon.Game.Services;
 using Dragon.Game.Instances;
+using Dragon.Game.Network.Senders;
 
 namespace Dragon.Game.Manager;
 
-public class SkillManager {
-    public IPlayer? Player { get; init; }
-    public IDatabase<Skill>? Skills { get; init; }
-    public IDatabase<Passive>? Passives { get; init; }
-    public IPacketSender? PacketSender { get; init; }
-    public InstanceService? InstanceService { get; init; }
+public sealed class SkillManager {
+    public ContentService? ContentService { get; private set; }
+    public InstanceService? InstanceService { get; private set; }
+    public PacketSenderService? PacketSenderService { get; private set; }
 
-    public void LearnFromInventory(int index, Item item) {
+    private const int StarterLevel = 1;
+
+    public SkillManager(IServiceContainer services) {
+        new ServiceInjector(services).Inject(this);
+    }
+
+    public void LearnFromInventory(IPlayer player, int index, Item item) {
         var added = false;
         var alreadyUsed = false;
 
         var id = item.SkillId;
 
-        if (Skills is not null) {
-            if (Skills.Contains(id)) {
-                var skill = Skills[id]!;
+        var sender = GetPacketSender();
+        var dbSkill = GetDatabaseSkill();
 
-                if (skill.Type == SkillType.Active) {
-                    if (!Player!.Skills.Contains(id)) {
+        if (dbSkill.TryGet(id, out var skill)) {
+            if (skill.Type == SkillType.Active) {
+                if (!player.Skills.Contains(id)) {
 
-                        added = true;
+                    added = true;
 
-                        var inventory = Player!.Skills.Add(id, 1);
+                    var inventory = player.Skills.Add(id, StarterLevel);
 
-                        AllocateSkills();
+                    AllocateSkills(player);
 
-                        AllocatePassives();
+                    AllocatePassives(player);
 
-                        PacketSender!.SendSkillUpdate(Player, inventory);
-                    }
-                    else {
-                        alreadyUsed = true;
-                    }
+                    sender.SendSkillUpdate(player, inventory);
                 }
-                else if (skill.Type == SkillType.Passive) {
-                    if (!Player!.Passives.Contains(id)) {
+                else {
+                    alreadyUsed = true;
+                }
+            }
+            else if (skill.Type == SkillType.Passive) {
+                if (!player.Passives.Contains(id)) {
 
-                        added = true;
+                    added = true;
 
-                        var inventory = Player!.Passives.Add(id, 1);
+                    var inventory = player.Passives.Add(id, 1);
 
-                        AllocateSkills();
+                    AllocateSkills(player);
 
-                        AllocatePassives();
+                    AllocatePassives(player);
 
-                        Player!.Passives.UpdateAttributes();
+                    player.Passives.UpdateAttributes();
 
-                        SendAttributes();
+                    SendAttributes(sender, player);
 
-                        PacketSender!.SendPassiveUpdate(Player, inventory);
-                    }
-                    else {
-                        alreadyUsed = true;
-                    }
+                    sender.SendPassiveUpdate(player, inventory);
+                }
+                else {
+                    alreadyUsed = true;
                 }
             }
         }
 
         if (alreadyUsed) {
-            PacketSender!.SendMessage(SystemMessage.YouAlreadyLearnedSkill, QbColor.BrigthRed, Player!);
+            sender.SendMessage(SystemMessage.YouAlreadyLearnedSkill, QbColor.BrigthRed, player);
         }
 
         if (added) {
-            var inventory = Player!.Inventories.FindByIndex(index);
+            var inventory = player.Inventories.FindByIndex(index);
 
             if (inventory is not null) {
                 inventory.Value--;
@@ -83,49 +87,45 @@ public class SkillManager {
                     inventory.Clear();
                 }
 
-                PacketSender!.SendInventoryUpdate(Player, index);
+                sender.SendInventoryUpdate(player, index);
             }
 
-            PacketSender!.SendMessage(SystemMessage.YouLearnedSkill, QbColor.BrigthGreen, Player!, new string[] { id.ToString() });
+            sender.SendMessage(SystemMessage.YouLearnedSkill, QbColor.BrigthGreen, player, new string[] { id.ToString() });
         }
     }
 
-    public void AllocateSkills() {
-        if (Skills is not null) {
-            var inventories = Player!.Skills.ToList();
+    public void AllocateSkills(IPlayer player) {
+        var inventories = player.Skills.ToList();
 
-            foreach (var inventory in inventories) {
-                inventory.ClearData();
+        var dbSkill = GetDatabaseSkill();
 
-                if (Skills.Contains(inventory.SkillId)) {
-                    var skill = Skills[inventory.SkillId]!;
-                    inventory.AllocateData(skill);
+        foreach (var inventory in inventories) {
+            inventory.ClearData();
+
+            if (dbSkill.TryGet(inventory.SkillId, out var skill)) {
+                inventory.AllocateData(skill);
+            }
+        }
+    }
+
+    public void AllocatePassives(IPlayer player) {
+        var inventories = player.Passives.ToList();
+
+        var dbSkill = GetDatabaseSkill();
+        var dbPassive = GetDatabasePassive();
+
+        foreach (var inventory in inventories) {
+            if (dbSkill.TryGet(inventory.PassiveId, out var skill)) {
+                if (dbPassive.TryGet(skill.PassiveId, out var passive)) {
+                    ApplyPassive(player, passive, inventory.PassiveLevel);
                 }
-            }
-        }
+            }     
+        } 
     }
 
-    public void AllocatePassives() {
-        if (Passives is not null && Skills is not null) {
-            var inventories = Player!.Passives.ToList();
-
-            foreach (var inventory in inventories) {
-                if (Skills.Contains(inventory.PassiveId)) {
-                    var skill = Skills[inventory.PassiveId]!;
-
-                    if (Passives.Contains(skill.PassiveId)) {
-                        var passive = Passives[skill.PassiveId]!;
-
-                        ApplyPassive(passive, inventory.PassiveLevel);
-                    }
-                }
-            }
-        }
-    }
-
-    private void ApplyPassive(Passive passive, int level) {
+    private void ApplyPassive(IPlayer player, Passive passive, int level) {
         if (passive.PassiveType == PassiveType.Improvement) {
-            var inventories = Player!.Skills.ToList();
+            var inventories = player.Skills.ToList();
 
             foreach (var inventory in inventories) {
                 if (inventory.SkillId == passive.SkillId) {
@@ -135,28 +135,41 @@ public class SkillManager {
         }
     }
 
-    private void SendAttributes() {
-        Player!.AllocateAttributes();
-        PacketSender!.SendAttributes(Player);
+    private void SendAttributes(IPacketSender sender, IPlayer player) {
+        player.AllocateAttributes();
 
-        var instance = GetInstance();
+        sender.SendAttributes(player);
+
+        var instance = GetInstance(player);
 
         if (instance is not null) {
-            PacketSender!.SendPlayerVital(Player, instance);
+            sender.SendPlayerVital(player, instance);
         }
         else {
-            PacketSender!.SendPlayerVital(Player);
+            sender.SendPlayerVital(player);
         }
     }
 
-    private IInstance? GetInstance() {
-        var instanceId = Player!.Character.Map;
+    private IInstance? GetInstance(IPlayer player) {
+        var instanceId = player.Character.Map;
         var instances = InstanceService!.Instances;
 
-        if (instances.ContainsKey(instanceId)) {
-            return instances[instanceId];
+        if (instances.TryGetValue(instanceId, out var instance)) {
+            return instance;
         }
 
         return null;
+    }
+
+    private IPacketSender GetPacketSender() {
+        return PacketSenderService!.PacketSender!;
+    }
+
+    private IDatabase<Skill> GetDatabaseSkill() {
+        return ContentService!.Skills;
+    }
+
+    private IDatabase<Passive> GetDatabasePassive() {
+        return ContentService!.Passives;
     }
 }
