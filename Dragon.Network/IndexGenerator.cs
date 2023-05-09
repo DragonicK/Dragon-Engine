886d29e49a137a1acaadef40cffd48871aebc9ab
Julio Sperandio <juliosperandio@hotmail.com>
2023-05-08 23:03:02 -0300

    Added lock when generate index.

--- Dragon.Network/IndexGenerator.cs
+++ Dragon.Network/IndexGenerator.cs
@@ -4,24 +4,30 @@ public sealed class IndexGenerator : IIndexGenerator {
     private readonly HashSet<int> indexes;
     private readonly int Maximum;
 
+    private readonly object _lock = new();
+
     public IndexGenerator(int maximum) {
         indexes = new HashSet<int>(maximum);
         Maximum = maximum;
     }
 
     public int GetNextIndex() {
-        for (var i = 1; i <= Maximum; ++i) {
-            if (!indexes.Contains(i)) {
-                indexes.Add(i);
+        lock (_lock) {
+            for (var i = 1; i <= Maximum; ++i) {
+                if (!indexes.Contains(i)) {
+                    indexes.Add(i);
 
-                return i;
+                    return i;
+                }
             }
-        }
 
-        return 0;
+            return 0;
+        }
     }
 
     public void Remove(int index) {
-        indexes.Remove(index);
+        lock (_lock) {
+            indexes.Remove(index);
+        }
     }
 }
